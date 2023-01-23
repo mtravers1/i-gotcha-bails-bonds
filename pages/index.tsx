746453b160da5a1eb44f0ca3deb446130e9334f0
Michael Travers <michael.travers@udc.edu>
2023-01-23 12:15:58 -0500

    added in forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -147,7 +147,7 @@ const Home = ()=>{
                     </p> */}
                     <span className={styled.maintext} style={{fontSize:h.fsize, color:h.color}} >
                     <p >The Go to Bail Bonds Agency <br/> 
-                    <span className={styled.pink}> Broward</span>, <span className={styled.pink}>Palm Beach</span> and <span className={styled.pink}>Miami</span></p>
+                    <span className={styled.pink} style={{color:"black"}}> Broward</span>, <span className={styled.pink} style={{color:"black"}}>Palm Beach</span> and <span style={{color:"black"}} className={styled.pink}>Miami</span></p>
                 </span>
                 </span>
                 <span className={styled.secondarytext}>
@@ -321,9 +321,9 @@ const Home = ()=>{
                     <div>
                         
 
-                    <button className='' >             <Link href="./form3" className=''>FLORIDA ADDENDUM TO BAIL BOND APPLICATION AND AGREEMENT</Link>
-
-</button>
+                    <button className='' >
+                        <Link href="./form3" className=''>FLORIDA ADDENDUM TO BAIL BOND APPLICATION AND AGREEMENT</Link>
+                    </button>
                     </div>
                     <div>
 
