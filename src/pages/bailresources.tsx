0282af49c7a6f88568061ede707a7d70978ab955
Jude Chinoso Violet <45746679+cvjude@users.noreply.github.com>
2023-02-20 20:19:43 +0100

    fix merge conflicts

--- src/pages/bailresources.tsx
+++ src/pages/bailresources.tsx
@@ -74,139 +74,96 @@ const Bailresources: FC = () => {
           ))}
         </ul>
       </PageSection>
-      {/* <PageSection>
-        
-      <h1 className="text-black text-2xl font-bold">
-            
-      <FormattedMessage id="Felony" />
-      </h1>
-   
-      {data.felony.map=((i,index)=>(
-       <>
-       <p>{i.title}</p>
-       </>
-      ))
-          }
-       
-      </PageSection> */}
-
-
-
 
       <PageSection>
-        <p className='m-6'>A state crime is any offense that breaks a law put into place by the state legislature. State crimes cover a long list of offenses and are
-           investigated and administered by state law enforcement and municipal courts, as well as state and city attorneys. 
-          Crimes grouped as state would be offenses in violation to state laws, such as domestic violence, assault and battery, stalking, and homicide.</p>
-      <h1 className="text-black text-2xl font-bold">
-
-      <FormattedMessage id="Felony" />
-      </h1>
-      <p>A felony is any criminal offense which is punishable by more than one year in state prison or federal 
-        prison and a fine of more than $1,000, Capital Felonies — An example of a capital felony is first-degree murder.</p>
+        <p className="m-6">
+          A state crime is any offense that breaks a law put into place by the
+          state legislature. State crimes cover a long list of offenses and are
+          investigated and administered by state law enforcement and municipal
+          courts, as well as state and city attorneys. Crimes grouped as state
+          would be offenses in violation to state laws, such as domestic
+          violence, assault and battery, stalking, and homicide.
+        </p>
+        <h1 className="text-black text-2xl font-bold">
+          <FormattedMessage id="Felony" />
+        </h1>
+        <p>
+          A felony is any criminal offense which is punishable by more than one
+          year in state prison or federal prison and a fine of more than $1,000,
+          Capital Felonies — An example of a capital felony is first-degree
+          murder.
+        </p>
         <h1 className="text-xl text-blue font-bold mb-3  mt-3">
-                <FormattedMessage id="Life Felonies" />
-              </h1>
-              <p>
-              These can include crimes such as aggravated sexual assault.
-              </p>
-              <h1 className="text-xl text-blue font-bold mb-3  mt-3">
-                <FormattedMessage id="First-degree Felonies " />
-              </h1>
-              <p>
-              An example of a first-degree felony is aggravated assault of a police officer while that officer is on duty, which is the most severe degree after capital and life felonies.
-These top three are related to profoundly serious crimes. Second and third-degree felonies are serious as well, and all require legal representation from a criminal defense attorney.
-What is a second-degree felony in Florida?
+          <FormattedMessage id="Life Felonies" />
+        </h1>
+        <p>These can include crimes such as aggravated sexual assault.</p>
+        <h1 className="text-xl text-blue font-bold mb-3  mt-3">
+          <FormattedMessage id="First-degree Felonies " />
+        </h1>
+        <p>
+          An example of a first-degree felony is aggravated assault of a police
+          officer while that officer is on duty, which is the most severe degree
+          after capital and life felonies. These top three are related to
+          profoundly serious crimes. Second and third-degree felonies are
+          serious as well, and all require legal representation from a criminal
+          defense attorney. What is a second-degree felony in Florida?
+        </p>
+        <h1 className="text-xl text-blue font-bold mb-3  mt-3">
+          <FormattedMessage id="Second-degree Felonies " />
+        </h1>
+        <p>
+          A drug offense such as selling marijuana to a minor would be an
+          example of a second-degree felony in Florida. What is a 3rd-degree
+          felony in Florida?
+        </p>
+        <h1 className="text-xl text-blue font-bold mb-3 mt-3">
+          <FormattedMessage id="Third-degree Felonies " />
+        </h1>
+        <p>
+          Examples of third-degree felonies in Florida include grand theft,
+          felony battery, and a third-time DUI, which took place within ten
+          years of a prior DUI conviction. There are multiple types of crimes
+          that constitute a felony charge in Florida, such as:
+        </p>
+        <ul className="list-disc list-inside ml-6">
+          <li>Murder</li>
+          <li>Rape and other sex crimes</li>
+          <li>Aggravated Battery & Aggravated Assault</li>
+          <li>Child Abuse</li>
+          <li>Incest</li>
+          <li>Domestic Violence</li>
+          <li>Stalking</li>
+          <li>Burglary</li>
+          <li>Grand theft</li>
+          <li>Kidnapping</li>
+          <li>Battery on a law enforcement officer or firefighter</li>
+          <li>Resisting a law enforcement officer using violence</li>
+          <li>
+            Possession of a controlled substance (not including marijuana under
+            20 grams)
+          </li>
+          <li>Drug trafficking</li>
+          <li>Robbery</li>
+          <li>Carrying a concealed firearm</li>
+        </ul>
 
-              </p>
-              <h1 className="text-xl text-blue font-bold mb-3  mt-3">
-                <FormattedMessage id="Second-degree Felonies " />
-              </h1>
-              <p>
-              A drug offense such as selling marijuana to a minor would be an example of a second-degree felony in Florida.
-What is a 3rd-degree felony in Florida?
-              </p>
-              <h1 className="text-xl text-blue font-bold mb-3 mt-3">
-                <FormattedMessage id="Third-degree Felonies " />
-              </h1>
-              <p>
-              Examples of third-degree felonies in Florida include grand theft, felony battery, and a third-time DUI,
-               which took place within ten years of a prior DUI conviction.   
-              There are multiple types of crimes that constitute a felony charge in Florida, such as:
-              </p>
-              <ul className='list-disc list-inside ml-6'>
-                <li>
-                Murder
-                </li>
-                <li>
-Rape and other sex crimes
-</li>
-<li>
-Aggravated Battery & Aggravated Assault
-</li>
-<li>
-Child Abuse
-</li>
-<li>
-Incest
-</li>
-<li>
-Domestic Violence
-</li>
-<li>
-Stalking
-</li>
-<li>
-Burglary
-</li>
-<li>
-Grand theft
-</li>
-<li>
-Kidnapping
-</li>
-<li>
-Battery on a law enforcement officer or firefighter
-</li>
-<li>
-Resisting a law enforcement officer using violence
-</li>
-<li>
-Possession of a controlled substance (not including marijuana under 20 grams)
-</li>
-<li>
-Drug trafficking
-</li>
-<li>
-Robbery
-</li>
-<li>
-Carrying a concealed firearm
-</li>
-              
-              </ul>
-
-              <h1 className="text-xl text-blue font-bold mb-3 mt-3">
-                <FormattedMessage id="Nebbia Hearing" />
-
-             
-              </h1>
-                     <p>A Nebbia hearing, also known as a bail source hearing, is a court procedure during which you must disclose the source of funds used to acquire the assets 
-                      you are posting for bail. 
-                      If you convince the court that those assets were obtained with legitimate funds, you may be released.</p>
-
+        <h1 className="text-xl text-blue font-bold mb-3 mt-3">
+          <FormattedMessage id="Nebbia Hearing" />
+        </h1>
+        <p>
+          A Nebbia hearing, also known as a bail source hearing, is a court
+          procedure during which you must disclose the source of funds used to
+          acquire the assets you are posting for bail. If you convince the court
+          that those assets were obtained with legitimate funds, you may be
+          released.
+        </p>
       </PageSection>
 
-     
-      
-
-                    
-      
       <PageSection>
         <h1 className="text-black text-2xl font-bold">
           <span className="text-red-500">*</span>{' '}
           <FormattedMessage id="bail-resources.info.bonds-def.title" />
         </h1>
-        
 
         <div className="mt-5">
           {data.bailBondsDef.map((item: any, index: number) => (
